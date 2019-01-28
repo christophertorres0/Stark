163362d61bbf983edd6d4ead2c28943287919f09
christophertorres0 <31553679+christophertorres0@users.noreply.github.com>
2019-01-28 13:25:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
-// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+
 
 // "el" stands for the "element" that was just clicked
+
+
 function allOpaque(el){
-	document.getElementById("dog2").classList.add("opaque");
-	document.getElementById("dog").classList.add("opaque");
+
+
+	document.getElementById("something").classList.add("opaque");
+
+
+	document.getElementById("super").classList.add("opaque");
+
+
+	
+
 
 }
+
+
+
+
+
+
 
 // "el" stands for the "element" that was just clicked
+
+
 function removeColor(el){
+
+
 	document.getElementById("pats-column").classList.remove("pats-silver");
+
+
 	document.getElementById("rams-column").classList.remove("rams-blue");
-}
 
-function showTrophy(el){
-	document.getElementById("dog2").src = "images/lombardi_trophy.png";
-	document.getElementById("mid-Column").src = "images/lombardi_trophy.png";
-	document.getElementById("dog").src = "images/lombardi_trophy.png";
+
+	
+
 
 }
+
+
+
+
+
+
+
+function showtrophy(el){
+
+
+	document.getElementById('bye').src = "images/1trophy.png";
+
+
+	document.getElementById('hey').src = "images/1trophy.png";
+
+
+	document.getElementById('11trophy').src = "images/1trophy.png";
+
+
+}
+
